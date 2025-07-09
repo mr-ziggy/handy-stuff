91667d0a5167e4fa24154d4866f8e60a1ee1b499
Z <zbigniew@mcxess.com>
2025-07-09 09:52:40 +0200

    complete

--- debian-base-config.sh
+++ debian-base-config.sh
@@ -22,3 +22,58 @@ systemctl restart ssh.service
 apt-get --purge -y remove rsyslog && apt-get update && apt-get -y install acl bootlogd bzip2 apt-transport-https curl dnsutils htop iftop iotop iptraf less links lsb-release lynx man mc ncdu net-tools psmisc screen sudo sysstat tcpdump telnet traceroute tshark unzip zip lsof socat gnupg2 wget syslog-ng vlan jq
 
 [ $(dmidecode -s system-manufacturer | grep -c "^VMware") == "1" ] && apt-get -y install open-vm-tools || (echo "Not VMWare, skipping VM Tools..." && apt-get -y install ifenslave)
+
+
+# Disable console cleanup after boot and enable verbose init
+cp /lib/systemd/system/getty@.service /lib/systemd/system/getty@.service.old
+sed -ie "s/^TTYVTDisallocate=yes$/TTYVTDisallocate=no/" /lib/systemd/system/getty@.service
+
+sed -ie "s/^\(GRUB_CMDLINE.*\)quiet\(.*\)$/\1\2/g" /etc/default/grub
+
+# Disable IPv6
+
+sed -ie "s/^\(GRUB_CMDLINE.*\)ipv6.disable=[0|1]\(.*\)$/\1\2/g" /etc/default/grub && \
+sed -ie "s/^\(GRUB_CMDLINE_LINUX=\"\)[ ]*\(.*\)$/\1ipv6.disable=1 \2/g" /etc/default/grub && \
+sed -i 's/[ ]*"/"/g' /etc/default/grub && \
+sed -i 's/[ ][ ]*/ /g' /etc/default/grub
+
+echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/00-ipv6-disable.conf && \
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.d/00-ipv6-disable.conf
+ 
+# Make dmesg quiet
+ 
+echo -e "@reboot\troot\tdmesg --console-off" > /etc/cron.d/dmesg-quiet
+
+# Change ls timestamps to ISO
+
+echo "export TIME_STYLE=full-iso" > /etc/profile.d/time-style.sh
+chmod 775 /etc/profile.d/time-style.sh
+
+
+> /etc/profile.d/aliases.sh 
+
+echo "alias historyoff='set +o history'" >> /etc/profile.d/aliases.sh 
+echo "alias historyon='set -o history'" >> /etc/profile.d/aliases.sh
+echo "alias ll='ls -lh'" >> /etc/profile.d/aliases.sh 
+echo "alias lla='ls -lha'" >> /etc/profile.d/aliases.sh 
+echo "alias ssu='sudo su -'" >> /etc/profile.d/aliases.sh 
+echo alias ver=\'lsb_release -a \| sed \"s/^\\\(Release.*\\\)$/\\1 \($\(cat /etc/debian_version\)\)/g\"\' >> /etc/profile.d/aliases.sh
+
+chmod 775 /etc/profile.d/aliases.sh
+
+# Get rid of some welcome messages
+ 
+> /etc/issue
+> /etc/issue.net
+> /etc/motd
+ 
+# Get rid of some error messages
+  
+echo "blacklist floppy" > /etc/modprobe.d/floppy.conf
+
+[ $(dmidecode -s system-manufacturer | grep -c "^VMware") == "1" ] && (echo "blacklist i2c_piix4" > /etc/modprobe.d/smbus.conf) || echo "Not VMWare, skipping..."
+[ $(dmidecode -s system-manufacturer | grep -c "^VMware") == "1" ] && (find /sys -name *max_write_same_blocks -exec echo w {} - - - - 0 \; > /etc/tmpfiles.d/max_write_same_blocks.conf) || echo "Not VMWare, skipping..."
+ 
+# Refresh GRUB
+ 
+depmod -a && update-initramfs -u && update-grub2
